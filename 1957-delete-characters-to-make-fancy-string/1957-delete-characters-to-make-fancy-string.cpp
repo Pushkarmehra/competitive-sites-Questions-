8c681a44432d1aee99d74a01a8f6efa2b62c8bb2
Pushkar <pushkarjeemain@gmail.com>
2025-07-22 04:21:18 +0530

    Time: 52 ms (11.36%), Space: 44.6 MB (39.84%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -1,40 +1,26 @@
-#include <queue>
 #include <string>
 using namespace std;
 
 class Solution {
 public:
     string makeFancyString(string s) {
-        if (s.empty()) return s;
-        
-        queue<char> qu;
         string result;
-        char prev;
-        int count = 0;
-        
-        for (int i = 0; i < s.size(); i++) {
-            if (i == 0) {
-                qu.push(s[0]);
-                prev = s[0];
-                count = 1;
-            } else {
-                if (prev == s[i]) {
-                    count++;
-                    if (count <= 2) { 
-                        qu.push(s[i]);
-                    }
-                } else {
-                    count = 1; 
-                    prev = s[i]; 
-                    qu.push(s[i]); 
-                }
+        result.push_back(s[0]);
+        int freq =1;
+    for(int i=1;i<s.size();i++){
+        if(result.back()==s[i]){
+            result.push_back(s[i]);
+            freq++;
+            if(freq==3){
+                result.pop_back();
+                freq--;
             }
         }
-        while (!qu.empty()) { 
-            result += qu.front(); 
-            qu.pop();
+        else{
+            freq=1;
+            result.push_back(s[i]);
         }
-        
-        return result;
+    }
+    return result;
     }
 };

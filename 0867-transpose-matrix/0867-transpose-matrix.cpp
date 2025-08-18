fe522fa03aa690d1b35a9cb38877f9dbaa2836df
Pushkar <pushkarjeemain@gmail.com>
2025-08-18 14:38:26 +0530

    Time: 0 ms (100%), Space: 15.2 MB (35.39%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<vector<int>> n(cols, vector<int>(rows));
+        for(int i=0;i<matrix.size();i++){
+        for(int j=0;j<matrix[i].size();j++){
+            n[j][i] = matrix[i][j];
+        }
+        
+        }
+        return n;
+    }
+};

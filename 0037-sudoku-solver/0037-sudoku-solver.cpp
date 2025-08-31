aeee176866d862ea79201e583061f5887c87870b
Pushkar <pushkarjeemain@gmail.com>
2025-09-01 02:19:28 +0530

    Time: 251 ms (71.22%), Space: 8.7 MB (87.8%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+    
+private:
+    bool solve(vector<vector<char>>& board) {
+        // Find the first empty cell
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    // Try digits 1-9
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            
+                            // Recursively try to solve the rest
+                            if (solve(board)) {
+                                return true;
+                            }
+                            
+                            // Backtrack if solution doesn't work
+                            board[row][col] = '.';
+                        }
+                    }
+                    // If no digit works, return false
+                    return false;
+                }
+            }
+        }
+        // If no empty cells found, puzzle is solved
+        return true;
+    }
+    
+    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+        // Check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == num) {
+                return false;
+            }
+        }
+        
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+        
+        // Check 3x3 box
+        int boxRow = (row / 3) * 3;
+        int boxCol = (col / 3) * 3;
+        for (int i = boxRow; i < boxRow + 3; i++) {
+            for (int j = boxCol; j < boxCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};

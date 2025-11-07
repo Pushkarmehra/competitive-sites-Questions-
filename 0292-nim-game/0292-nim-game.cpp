7b5031149878f4da1fd1a36de93fecd9e26b3cd0
Pushkar <pushkarjeemain@gmail.com>
2025-11-07 22:08:09 +0530

    Time: 0 ms (100%), Space: 7.8 MB (17.53%) - LeetHub

--- 0292-nim-game/0292-nim-game.cpp
+++ 0292-nim-game/0292-nim-game.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     bool canWinNim(int n) {
-         if(n==1||n==2||n==3){
-            return true;
-        }
-        else{
-        if(n%4==0){
-            return false;
-        }
-        else
-        return true;
-        }
-        return false;
+        return n % 4 != 0;
     }
 };

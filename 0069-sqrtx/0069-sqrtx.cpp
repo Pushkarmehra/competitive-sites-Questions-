a1f747cc651f57e7f455429c14b3ba9371418750
Pushkar <pushkarjeemain@gmail.com>
2026-01-13 00:11:36 +0530

    Time: 0 ms (100%), Space: 8.6 MB (48.89%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        for(int i=0;i<=x;i++){
-            if(pow(i,2)>x){
-                return i-1;
+        int left=0;
+        int right=x;
+       while(left<=right){
+        int middle=left+(right-left)/2;
+            if(pow(middle,2)>x){
+                if(pow(middle-1,2)<x){
+                return middle-1;}
+                else{
+                    right=middle-1;
+                }
             }
-            else if(pow(i,2)==x){
-                return i;
+            else if(pow(middle,2)==x){
+                return middle;
+            }
+            else{
+                left=middle+1;
             }
         }
         return x;

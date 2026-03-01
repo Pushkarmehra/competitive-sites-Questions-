b72cc64a558aa458e2b42fcd999b1ef5c87139fb
Pushkar <pushkarjeemain@gmail.com>
2026-03-01 19:59:22 +0530

    Time: 38 ms (5.68%), Space: 16.5 MB (81.74%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        if(n.size()<0){
-            return 0;
-        }
-        char max='0';
-        for(int i=0;i<n.size();i++){
-            if(max<n[i])
-            max=n[i];
-        }
-        int result=max-'0';
-        return result;
+        sort(n.begin(),n.end());
+        int max=n[n.size()-1]-'0';
+        return max;
     }
 };

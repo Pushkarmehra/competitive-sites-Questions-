eb6968c1b7885782b303f7f976d0990dc4188fbb
Pushkar <pushkarjeemain@gmail.com>
2026-06-06 11:35:10 +0530

    Time: 0 ms (100%), Space: 19.1 MB (86.19%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -4,8 +4,8 @@ class Solution:
         cost=cost[::-1]
         price=0
         for i in range(0,len(cost),3):
-            price += cost[i]
-            if i + 1 < len(cost):
-                price += cost[i + 1]
+            price+=cost[i]
+            if i+1< len(cost):
+                price+=cost[i+1]
         
         return price
